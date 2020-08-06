940e909f4aa15f0fe2e65d0429ba1e248b646de2
flynnduism <dev@ronan.design>
2020-08-05 23:42:25 -0700

    adventures in css

--- themes/clean-landing/static/js/landing.js
+++ themes/clean-landing/static/js/landing.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
-  //Preloader
+  
+  // preloader
   $(window).on('load', function() { // makes sure the whole site is loaded 
     $('#status').fadeOut(); // will first fade out the loading animation 
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(350).css({'overflow':'visible'});
+
+    // party
+    $(".wow").each(function(i,el){
+      var tLeft = Math.floor(Math.random()*(window.innerWidth)+0),
+        tTop  = Math.floor(Math.random()*(window.innerHeight)+50);
+      $(el).css({position:'absolute', left: tLeft });
+    });
   });
+
+  $('.begone a.fa-pause').on('click', function(){
+    $('.krustparty').addClass('cease');
+    $('.controls').removeClass('active');
+    $('.begone a.fa-play').removeClass('hidden');
+    $(this).addClass('hidden');
+  });
+
+  $('.begone a.fa-play').on('click', function(){
+    $('.krustparty').removeClass('cease');
+    $('.controls').addClass('active');
+    $('.begone a.fa-pause').removeClass('hidden');
+    $(this).addClass('hidden');
+  });
+
 });
