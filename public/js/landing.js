87ade23fea3d61134f85bc2c9998d4318d1d7363
flynnduism <dev@ronan.design>
2020-08-06 00:04:12 -0700

    tidy up
    Signed-off-by: flynnduism <dev@ronan.design>

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
+  $.fn.randoParty = function(){ 
+    alert('You have successfully randomized a party wow!');
+
+    var tLeft = Math.floor(Math.random()*(window.innerWidth)+0),
+        tTop  = Math.floor(Math.random()*(window.innerHeight)+50);
+    $(el).css({position:'absolute', left: tLeft });
+  }
   
   // preloader
   $(window).on('load', function() { // makes sure the whole site is loaded 
@@ -6,7 +14,7 @@ $(document).ready(function(){
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
     $('body').delay(350).css({'overflow':'visible'});
 
-    // party
+    // start the party
     $(".wow").each(function(i,el){
       var tLeft = Math.floor(Math.random()*(window.innerWidth)+0),
         tTop  = Math.floor(Math.random()*(window.innerHeight)+50);
@@ -14,6 +22,7 @@ $(document).ready(function(){
     });
   });
 
+  // show or hide the party
   $('.begone a.fa-pause').on('click', function(){
     $('.krustparty').addClass('cease');
     $('.controls').removeClass('active');
@@ -26,6 +35,11 @@ $(document).ready(function(){
     $('.controls').addClass('active');
     $('.begone a.fa-pause').removeClass('hidden');
     $(this).addClass('hidden');
+  });
+
+  // add or remove wow stuff
+  $('.controls a.fa-plus').on('click', function(){
+    $(".krustparty").append("<div class=\"wow wow-wa\"></div>");
   });
 
 });
